bc10dcb27b83e1d4c7f0ff5e5e29c8d28424376f
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-03-05 18:26:24 +0800

    feat: add and structure Header's html content

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar">
+    <div class="logo-container">
+      <img src="../../assets/images/HOUSE168 LOGO03.png" alt="house168 logo" />
+    </div>
+    <div class="nav-links">
+      <a href="#" class="nav-link">關於我們</a>
+      <il class="dropdown-content">
+        <div class="dropdown-link-container">
+          <a href="#" class="dropdown-link">房屋變裝</a>
+        </div>
+        <div class="dropdown-link-container">
+          <div class="dropdown-link-container"></div>
+        </div>
+        <div class="dropdown-link-container">
+          <a href="#" class="dropdown-link">建商製作</a>
+        </div>
+        <div class="dropdown-link-container">
+          <a href="#" class="dropdown-link">LINE貼圖</a>
+        </div>
+        <div class="dropdown-link-container">
+          <a href="#" class="dropdown-link">銷售報告書</a>
+        </div>
+        <div class="dropdown-link-container">
+          <a href="#" class="dropdown-link">顧問型影音</a>
+        </div>
+        <div class="dropdown-link-container">
+          <a href="#" class="dropdown-link">浮水印</a>
+        </div>
+      </il>
+      <a href="#" class="nav-link">定價</a>
+      <a href="#" class="nav-link">房屋交易</a>
+    </div>
+    <div class="user-utility-menu">
+      <button class="login-btn">登入</button>
+      <button class="membership-btn">會員中心</button>
+      <button class="language-btn">
+        <img src="../../assets/images/language_icon.svg" alt="language-icon" />Language
+      </button>
+    </div>
+    <div class="action-buttons"></div>
+  </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+import TheHeader from '@/components/layout/TheHeader.vue';
+// import TheMain from '../layout/TheMain.vue';
+// import TheFooter from '../layout/TheFooter.vue';
+// import HeroBanner from '../components/HeroBanner.vue';
+// import FeatureGrid from '../components/FeatureGrid.vue';
+// import ServiceSlider from '../components/ServiceSlider.vue';
+// import PromotionBanner from '../components/PromotionBanner.vue';
+// import TestimonialSlider from '../components/TestimonialSlider.vue';
+// import PartnerSlider from '../components/PartnerSlider.vue';
+// import HighlightSection from '../components/HighlightSection.vue';
+</script>
 
 <template>
   <div id="app">
@@ -16,5 +27,3 @@
     <TheFooter />
   </div>
 </template>
-
-<style scoped></style>
