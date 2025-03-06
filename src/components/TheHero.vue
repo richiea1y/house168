553938b1a838416c05487f1b1bb8b5666ec5ccf3
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-03-06 23:49:59 +0800

    style: styling hero section

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -21,8 +21,70 @@
         </div>
       </div>
     </div>
-    <div class="hero-content2-container"></div>
-    <div class="hero-content3-container"></div>
+    <div class="hero-content2-container">
+      <div class="hero-content2-text-container1">
+        <div class="content2-text1-img img-container">
+          <img src="@/assets/images/content2-img1.png" alt="" />
+        </div>
+        <div class="content2-text1 text-container">
+          <p>創新科技 引領未來</p>
+          <p>
+            house168，不斷追求創新技術， 引領未來趨勢，為您提供卓越服務， 成就房仲夥伴的交易目標。
+          </p>
+        </div>
+      </div>
+      <div class="hero-content2-text-container2">
+        <div class="content2-text2-img img-container">
+          <img src="@/assets/images/content2-img2.png" alt="" />
+        </div>
+        <div class="content2-text2 text-container">
+          <p>儲值點數 終生有效</p>
+          <p>儲值點數，不受時間限制， 長期使用我們的服務， 充分發揮您的業務潛力。</p>
+        </div>
+      </div>
+      <div class="hero-content2-text-container3">
+        <div class="content2-text3-img img-container">
+          <img src="@/assets/images/content2-img3.png" alt="" />
+        </div>
+        <div class="content2-text3 text-container">
+          <p>專業服務 成就卓越</p>
+          <p>house168，您成功的一大助力， 資訊整合、深度服務， 有效幫助成為您最信賴的合作夥伴。</p>
+        </div>
+        <img src="@/assets/images/msg-img.png" alt="" class="msg-img" />
+      </div>
+    </div>
+    <div class="hero-content3-container">
+      <div class="tools-img-container">
+        <div class="tools-img1">
+          <img src="@/assets/images/before-interior.png" alt="" />
+        </div>
+        <div class="tools-img2">
+          <img src="@/assets/images/after-interior.png" alt="" />
+        </div>
+        <div class="tools-img3">
+          <img src="@/assets/images/before-interior-flag.png" alt="" />
+        </div>
+        <div class="tools-img4">
+          <img src="@/assets/images/after-interior-flag.png" alt="" />
+        </div>
+      </div>
+      <div class="tools-intro-container">
+        <div class="tools-intro1">
+          <div>
+            <p>主打工具-房屋變裝</p>
+            <p>房仲成交神器</p>
+          </div>
+          <div>
+            <p>透過house168的房屋變裝服務，我們利用最新的AI技術，</p>
+            <p>將空置或髒亂的房屋重新裝扮，為您呈現出煥然一新的風貌</p>
+            <p>無論是全新的成屋還是中古房屋，我們能夠重新設計裝潢，</p>
+            <p>讓每個空間焕然一新。這不僅能吸引更多潛在買家的注意，</p>
+            <p>還能加速房屋成交的速度，讓您的房地產業務更上一層樓。</p>
+          </div>
+        </div>
+        <button class="tools-intro2">瞭解更多</button>
+      </div>
+    </div>
   </div>
 </template>
 
