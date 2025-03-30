df9c89ca702444e828e494306765fb7e52bd10b1
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-03-31 00:23:46 +0800

    feat: style highlight card's flag

--- src/components/Highlight/HighlightCard.vue
+++ src/components/Highlight/HighlightCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="highlight__card">
     <div class="highlight__card-flag">Step 1.</div>
-    <div class="highlight__card-case"></div>
+    <HighlightCase />
     <div
       v-for="card in highlightCards"
       :key="card.id"
@@ -16,6 +16,8 @@
 </template>
 
 <script setup>
+import HighlightCase from './HighlightCase.vue';
+
 defineProps({
   highlightCards: Array
 });
@@ -26,21 +28,30 @@ defineProps({
 @use '../../styles/abstract/mixins';
 @use '../../styles/abstract/functions';
 
-$primary-color: #ff8817;
-$secondary-color: #55a3ec;
 .highlight__card {
   max-width: 1000px;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
   border-radius: 25px 15px 15px 15px;
+  display: flex;
+  position: relative;
+
+  &-flag {
+    max-width: 250px;
+    position: absolute;
+    top: -1px;
+    left: -2px;
+    font-size: functions.rem(22);
+    padding: 0.4em 1.5em;
+    background-color: #fcb33f;
+    color: #fff;
+    border-radius: 30px 0px 40px 0px;
+  }
 
   &-content {
     display: flex;
     flex-direction: column;
     max-width: 400px;
     padding: 1.5em;
-    // align-items: center;
-    // justify-content: center;
-    // gap: 20px;
 
     & > p {
       margin: 0;
@@ -49,7 +60,7 @@ $secondary-color: #55a3ec;
   &-title {
     font-size: functions.rem(22);
     font-weight: 900;
-    color: $primary-color;
+    color: variables.$primary-btn-color;
   }
   &-button {
     @include mixins.gradient-button('primary');
---
+++ src/components/Highlight/HighlightCase.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="highlight__card-case">
+    <img
+      src="@/assets/images/highlight-before.png"
+      alt="house168 行銷工具"
+      class="interior-image"
+    />
+    <img src="@/assets/images/double-arrow.png" alt="house168 行銷工具" />
+    <img src="@/assets/images/highlight-after.png" alt="house168 行銷工具" class="interior-image" />
+  </div>
+</template>
