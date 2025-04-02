e45760459c2c1fcc4236f3b442afc8bdaab885a9
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-02 15:01:57 +0800

    add: create a new component to replace TestimonialSlider

---
+++ src/components/FeedbackSlider/FeedbackSlider.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="testimonial-header">
+    <p>––&emsp;&ensp;與我們合作的夥伴都說&ensp;&emsp;––</p>
+  </div>
+</template>
+
+<script setup></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar/Navbar.vue';
 import TheHero from '@/components/Hero/TheHero.vue';
 import FeatureTools from '@/components/FeatureTools/FeatureTools.vue';
 import PromotionBanner from '@/components/PromotionBanner.vue';
+import FeedbackSlider from '@/components/FeedbackSlider/FeedbackSlider.vue';
 import TestimonialSlider from '@/components/TestimonialSlider.vue';
 import HighlightSection from '@/components/Highlight/HighlightSection.vue';
 import TheFooter from '@/components/TheFooter.vue';
@@ -14,6 +15,7 @@ import TheFooter from '@/components/TheFooter.vue';
     <TheHero />
     <FeatureTools />
     <PromotionBanner />
+    <FeedbackSlider />
     <TestimonialSlider />
     <HighlightSection />
     <TheFooter />
